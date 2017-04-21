7e9954ce6ce1a89e2142bdb7334bdbfc2578a7d5
cmtruong <cong-minh.truong@capgemini.com>
2017-04-21 17:08:49 +0200

    Insert all apis needed to build the project

---
+++ app/src/main/java/mitru/com/mytil/model/Note.java
@@ -0,0 +1,85 @@
+package mitru.com.mytil.model;
+
+/**
+ * Created by ctruong on 21/04/2017.
+ */
+public class Note {
+    private long id;
+
+    private String name;
+
+    private String content;
+
+    private String date;
+
+    private String author;
+
+    private boolean isReminder;
+
+    private String dateReminder;
+
+    public Note(String name, String content, String date, String author, boolean isReminder, String dateReminder) {
+        this.name = name;
+        this.content = content;
+        this.date = date;
+        this.author = author;
+        this.isReminder = isReminder;
+        this.dateReminder = dateReminder;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isReminder() {
+        return isReminder;
+    }
+
+    public void setReminder(boolean reminder) {
+        isReminder = reminder;
+    }
+
+    public String getDateReminder() {
+        return dateReminder;
+    }
+
+    public void setDateReminder(String dateReminder) {
+        this.dateReminder = dateReminder;
+    }
+}
